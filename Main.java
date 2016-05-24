447970839c7d981035bdc85648cb7833712021d9
Jasonvivi <jason.chen.bjtu@gmail.com>
2016-05-24 16:26:06 +0800

    init

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
